cf0dec89fb803908afbbc57ec5430d02469508ce
Lili Fehertoi <lfehertoi@gmail.com>
2020-08-31 23:43:35 +0200

    added traversal

--- board.py
+++ board.py
@@ -1,7 +1,9 @@
 #%%
 import numpy as np
+import random
 import matplotlib.pyplot as plt
 import collections
+import copy
 #%%
 class Board():
     def __init__(self, width, height):
@@ -10,48 +12,57 @@ class Board():
 
     def create_graph(self):
         nodes = collections.defaultdict(dict)
-        for i in range(self.height):
-            for j in range(self.width):
+        for i in range(self.width):
+            for j in range(self.height):
                 nodes[i][j] = []
 
         self.graph = nodes
 
     def add_edges(self):
 
-        on_board = lambda x, y: (x >= 0 & x <= self.width) & (y >= 0 & y <= self.height)
+        on_board = lambda x, y: ((x >= 0) and (x < self.width)) and ((y >= 0) and (y < self.height))
         legal = [(1, 2), (1, -2), (-1, 2), (-1, -2), (2, 1), (2, -1), (-2, 1), (-2, -1)]
 
-        for i in range(self.height):
-            for j in range(self.width):
+        for i in range(self.width):
+            for j in range(self.height):
                 for r, s in legal:
                     if on_board(i+r, j+s):
                         self.graph[i][j] += [(i+r, j+s)]
 
     def warnsdorff_graph(self):
-        paths = self.graph.copy()
-        for i in range(self.height):
-            for j in range(self.width):
-                paths[i][j] = len(self.graph[i][j])
+        paths = copy.deepcopy(self.graph)
+        for i in range(self.width):
+            for j in range(self.height):
+                paths[i][j] = len(paths[i][j])
 
         self.warnsdorff = paths
 
+    def traverse(self, start = None):
 
+        if not start:
+            i, j = random.randint(0, self.width-1), random.randint(0, self.height-1)
+        if start:
+            i, j = start
+        
+        neighbours = self.graph[i][j]
+        path = []
+        visited = set()
 
+        while neighbours != []:
+            path += [(i, j)]
+            w_index = [*zip(neighbours, [self.warnsdorff[r][s] for r, s in neighbours])]
+            w_index = sorted(w_index, key = lambda x: x[1])
+            next_square = w_index[0][0]
+            visited.add((i, j))
+        
+            i, j = next_square
+            neighbours = [x for x in self.graph[i][j] if x not in visited]
 
+        return path
 
 # %%
 chessboard = Board(5, 3)
-print(chessboard.width, chessboard.height)
-# %%
 chessboard.create_graph()
-# %%
-chessboard.graph
-# %%
 chessboard.add_edges()
-# %%
-chessboard.graph
-# %%
 chessboard.warnsdorff_graph()
-# %%
-chessboard.warnsdorff
-# %%
+chessboard.traverse()
